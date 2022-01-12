aba9cdd55783a99f3a6ad38f161a2eaa789f80fb
lucas <lucasucb2020@gmail.com>
2022-01-12 09:05:32 -0300

    Criando a classe department

---
+++ src/db/DBException.java
@@ -0,0 +1,16 @@
+package db;
+
+public class DBException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public DBException(String msg) {
+		super(msg);
+	}
+	
+	
+}
